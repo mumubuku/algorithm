e15a34e4bb92928a73be172accd4ac12ed1c5f9a
tianyunzhao <tianyunzhao@yanpai.com>
2024-04-30 18:02:32 +0800

    完善算法题目

---
+++ src/main/java/com/example/algorithm/find/eventual/safe/description/Soultion.java
@@ -0,0 +1,14 @@
+package com.example.algorithm.find.eventual.safe.description;
+
+/**
+ * Soultion
+ *
+ * @author mumu
+ * @date 2024/4/30
+ * @description TODO
+ */
+public class Soultion {
+    public static void main(String[] args) {
+
+    }
+}
